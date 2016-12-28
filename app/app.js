c6e2101bc3c7a346c7da6142175bcc11571ba134
aostrovsky <anatolyostrovsky@gmail.com>
2016-12-28 15:08:25 +0200

    Add time directive

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 import angular from 'angular';
 import 'angular-route';
-import IndexCtrl from './IndexCtrl';
 
 angular.module('app', [
   'ngRoute'
@@ -10,4 +9,26 @@ angular.module('app', [
     $routeProvider.otherwise({redirectTo: '/'});
   }
 ])
-.controller('IndexCtrl', IndexCtrl);
+.directive('timeDirective', () => {
+
+  const getDate = () => {
+    const date = new Date();
+    return addZero(date.getHours()) + ':' + addZero(date.getMinutes()) + ':' + addZero(date.getSeconds());
+  };
+
+  const addZero = (i) => {
+    if (i < 10) {
+      i = '0' + i;
+    }
+    return i;
+  };
+
+  const DELAY = 1000;
+
+  return {
+    compile: (el) => {
+      el.html(getDate());
+      setInterval(() => { el.html(getDate()); }, DELAY);
+    }
+  };
+});

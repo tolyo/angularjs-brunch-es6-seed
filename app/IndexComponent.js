eae8a8bdba6fbd8102e868b89c35014919f97dca
tolyo <anatolyostrovsky@gmail.com>
2019-04-23 22:13:01 +0300

    Update

--- app/IndexComponent.js
+++ app/IndexComponent.js
@@ -4,31 +4,31 @@ const DELAY = 1000;
 
 class IndexCtrl {
 
-	constructor($interval) {
-		this.currentTime = this.getDate();
-		this.$interval = $interval;
-	}
+  constructor($interval) {
+    this.currentTime = this.getDate();
+    this.$interval = $interval;
+  }
 
-	$onInit() {
-		this.$interval(() => {
-			this.currentTime = this.getDate();
-		}, DELAY);
-	}
+  $onInit() {
+    this.$interval(() => {
+      this.currentTime = this.getDate();
+    }, DELAY);
+  }
 
-	getDate() {
-		const date = new Date();
-		return this.addZero(date.getHours()) + ':' + this.addZero(date.getMinutes()) + ':' + this.addZero(date.getSeconds());
-	}
+  getDate() {
+    const date = new Date();
+    return this.addZero(date.getHours()) + ':' + this.addZero(date.getMinutes()) + ':' + this.addZero(date.getSeconds());
+  }
 
-	addZero(i) {
-		if (i < 10) i = '0' + i;
-		return i;
-	}
+  addZero(i) {
+    if (i < 10) i = '0' + i;
+    return i;
+  }
 }
 
 IndexCtrl.$inject = ['$interval'];
 
 export default {
-	template  : `Current time: {{ $ctrl.currentTime }}`,
-	controller: IndexCtrl
+  template  : 'Current time: {{ $ctrl.currentTime }}',
+  controller: IndexCtrl
 };
